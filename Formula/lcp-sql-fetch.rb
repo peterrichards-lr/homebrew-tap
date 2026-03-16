784788e6448aed5e72895953180a7d4147c229c0
Peter Richards <peter.richards@liferay.com>
2026-03-16 16:40:47 +0000

    feat: update lcp-sql-fetch v1.0.1

--- Formula/lcp-sql-fetch.rb
+++ Formula/lcp-sql-fetch.rb
@@ -1,8 +1,8 @@
 class LcpSqlFetch < Formula
   desc "A tool that executes local SQL scripts on a Liferay Cloud database via lcp shell and downloads the results."
   homepage "https://github.com/peterrichards-lr/lcp-sql-fetch"
-  url "https://github.com/peterrichards-lr/lcp-sql-fetch/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "f51726f6579bccbe10963fa816250cd0b9eab712b71844e36fdbe93b15603f40"
+  url "https://github.com/peterrichards-lr/lcp-sql-fetch/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "905a3e2eb854317f1654051bd6c7536219fc2689aba0aefe7931757c7f365d44"
   license "MIT"
 
   depends_on "rust" => :build
